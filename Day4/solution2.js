7111d12cb47eda74df5614ec4ee6507d0cfc3812
Caitlin Yih <caitlin@backbase.com>
2023-12-04 21:31:01 +0000

    Day 4

--- Day4/solution2.js
+++ Day4/solution2.js
@@ -1,105 +1,45 @@
 /* START OF DATA PROCESSING */
 const fs = require('fs');
 
-let cards
+let scratchcards
 try {
-    let input = fs.readFileSync('sampleInput.txt', 'utf8')
-    cards = input.split('\n')
+    let input = fs.readFileSync('input.txt', 'utf8')
+    scratchcards = input.split('\n')
 } catch (err) {
     console.error(err);
 }
 /* END OF DATA PROCESSING */
 
-let cardMap = new Map();
 
-for (let i = 0; i < cards.length; i++) {
-    let cardNumber = parseInt(cards[i].split(':')[0].split(' ')[1])
+let scratchcardCounter = new Map();
 
-    const numbers = cards[i].split(':')[1].split('|')
-    const winningNumbers = numbers[0].trim().split(' ')
-    const playingNumbers = numbers[1].trim().split(' ')
-
-    let matchCount = 0
-    
-    for (let i = 0; i < playingNumbers.length; i++) {
-        if (playingNumbers[i].trim().length === 0) {
-            continue
-        }
-       
-        if (winningNumbers.includes(playingNumbers[i])) {
-            matchCount++
-        }
-    }
-
-    cardMap.set(cardNumber, matchCount)
+for (let i = 0; i < scratchcards.length; i++) {
+    let cardNumber = parseInt(scratchcards[i].split(':')[0].split(/\s+/)[1])
+    scratchcardCounter.set(cardNumber, 1)
 }
 
-console.log(cardMap)
+for (let i = 0; i < scratchcards.length; i++) {
+    let scratchcardNumber = parseInt(scratchcards[i].split(':')[0].split(/\s+/)[1])
+    let scratchcardCount = scratchcardCounter.get(scratchcardNumber)
 
+    const numbers = scratchcards[i].split(':')[1].split('|')
+    const winningNumbers = numbers[0].trim().split(/\s+/)
+    const playingNumbers = numbers[1].trim().split(/\s+/)
 
-
-let cardPile = [1,2,3,4,5,6]
-
-for (let i = 0; i < cards.length; i++) {
-    const numbers = cards[i].split(':')[1].split('|')
-    const winningNumbers = numbers[0].trim().split(' ')
-    const playingNumbers = numbers[1].trim().split(' ')
-
-    let cardNumber = parseInt(cards[i].split(':')[0].split(' ')[1])
-    
-    for (let i = 0; i < playingNumbers.length; i++) {
-        if (playingNumbers[i].trim().length === 0) {
-            continue
-        }
-       
+    for (let i = 0; i < playingNumbers.length; i++) {       
         if (winningNumbers.includes(playingNumbers[i])) {
-            cardPile.push(cardNumber + 1)
-            cardNumber++
+            let nextScratchcardNumber = scratchcardNumber + 1
+            let nextScratchcardCount = scratchcardCounter.get(nextScratchcardNumber)
+
+            scratchcardCounter.set(nextScratchcardNumber, nextScratchcardCount + scratchcardCount)
+
+            scratchcardNumber++
         }
     }
 }
 
 let sum = 0
-console.log(cardPile)
-
-for (let i = 0; i < cardPile.length; i++) {
-    const cardNumber = cardPile[i]
-    const cardsGenerated = cardMap.get(cardNumber)
-    console.log(cardNumber + ", " + cardsGenerated)
-    sum += cardsGenerated
-}
-
+scratchcardCounter.forEach(value => {
+    sum += value;
+});
 console.log(sum)
-
-
-
-
-/*
-let cardsCopy = Array.from(cards)
-
-for (let i = 0; i < cards.length; i++) {
-    if (cards[i] === undefined || cards[i].trim().length === 0) {
-        continue
-    }
-
-    let cardNumber = parseInt(cards[i].split(':')[0].split(' ')[1])
-
-    const numbers = cards[i].split(':')[1].split('|')
-    const winningNumbers = numbers[0].trim().split(' ')
-    const playingNumbers = numbers[1].trim().split(' ')
-    console.log(playingNumbers)
-    
-    for (let i = 0; i < playingNumbers.length; i++) {
-        if (playingNumbers[i].trim().length === 0) {
-            continue
-        }
-       
-        if (winningNumbers.includes(playingNumbers[i])) {
-            cards.push(cardsCopy[cardNumber])
-            cardNumber++
-        }
-    }
-}
-
-console.log(cards.length)
-*/
